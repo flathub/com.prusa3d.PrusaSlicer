6ddf44c00b134a37c622add6487c17e20473008d
Jason Scurtu <code@xarbit.com>
2021-11-27 19:20:39 +0100

    add prusaslicer-bin & use logic for locale + theme

--- set-dark-theme-variant.py
+++ set-dark-theme-variant.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
-import os
-import time
-import subprocess
-import sys
-
 import Xlib
 import Xlib.display
+import time
+import subprocess
+import os
+import sys
 
 disp = Xlib.display.Display()
 root = disp.screen().root
 
 NET_CLIENT_LIST = disp.intern_atom('_NET_CLIENT_LIST')
-
 
 def set_theme_variant(window_titles, variant):
     # Loop though all of the windows and look for one that matches one
@@ -33,16 +31,15 @@ def set_theme_variant(window_titles, variant):
                 pass
     return False
 
-
 def set_theme_variant_by_window_title(title, variant):
     # Pretty self explanatory. Use subprocess to call
     # xprop and set the variant.
     try:
         subprocess.call(['xprop', '-name', title, '-f', '_GTK_THEME_VARIANT', '8u',
-                         '-set', '_GTK_THEME_VARIANT', variant], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            '-set', '_GTK_THEME_VARIANT', variant], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         return True
     except:
-        return False
+        return False       
 
 
 def fallback(window_titles, variant):
@@ -54,33 +51,32 @@ def fallback(window_titles, variant):
     for title in window_titles:
         set_theme_variant_by_window_title(title, variant)
 
-
 if __name__ == '__main__':
-    if 'GTK_THEME' in os.environ:
-        if os.environ['GTK_THEME'] == "Adwaita:dark":
-            start = time.time()
-            # Listen for X Property Change events.
-            root.change_attributes(event_mask=Xlib.X.PropertyChangeMask)
-            window_titles = ('PrusaSlicer-')
-            variant = 'dark'
-            # In the unlikely event that our window shows up before
-            # the script starts don't start a while loop.
-            if not set_theme_variant(window_titles, variant):
-                # Basically give our window 2 secs to show up.
-                # If it doesn't just fallback to the old way of blindly
-                # trying to set the theme variant
-                while True:
-                    if time.time() - start <= 2:
-                        # disp.next_event() blocks if no events are
-                        # queued. In combination with while True
-                        # it creates a very simple event loop.
-                        disp.next_event()
-                        # We only try to set the variant after
-                        # there has been a Property Change event.
-                        if set_theme_variant(window_titles, variant):
-                            break
-                    else:
-                        fallback(window_titles, variant)
-                        break
-        else:
-            sys.exit(0)
+
+    if os.environ.get('PRUSA_SLICER_DARK_THEME') != 'true':
+        sys.exit(0)
+
+    start = time.time()
+    # Listen for X Property Change events.
+    root.change_attributes(event_mask=Xlib.X.PropertyChangeMask)
+    window_titles = ('PrusaSlicer')
+    variant = 'dark'
+    # In the unlikely event that our window shows up before
+    # the script starts don't start a while loop.
+    if not set_theme_variant(window_titles, variant):
+        # Basically give our window 2 secs to show up.
+        # If it doesn't just fallback to the old way of blindly
+        # trying to set the theme variant
+        while True:
+            if time.time() - start <= 2:
+                # disp.next_event() blocks if no events are
+                # queued. In combination with while True
+                # it creates a very simple event loop.
+                disp.next_event()
+                # We only try to set the variant after
+                # there has been a Property Change event.
+                if set_theme_variant(window_titles, variant):
+                    break
+            else:
+                fallback(window_titles, variant)
+                break
